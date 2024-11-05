fda1d1d9dbdc26841d4fa60448123d70b57ffa3e
Alon Shprung <alonshp1@gmail.com>
2024-11-05 16:03:49 +0200

    add

---
+++ UnitPushProvisioning/UnitPPClass.swift
@@ -0,0 +1,18 @@
+//
+//  UnitPPClass.swift
+//  UnitPushProvisioning
+//
+//  Created by Alon Shprung on 05/11/2024.
+//
+
+import Foundation
+import UnitCore
+
+public class UnitPPClass {
+    public init() {}
+
+    public func test() {
+        print("UnitPPClass test")
+        UnitCoreClass().test()
+    }
+}
---
+++ UnitPushProvisioning/UnitPushProvisioning.h
@@ -0,0 +1,18 @@
+//
+//  UnitPushProvisioning.h
+//  UnitPushProvisioning
+//
+//  Created by Alon Shprung on 05/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitPushProvisioning.
+FOUNDATION_EXPORT double UnitPushProvisioningVersionNumber;
+
+//! Project version string for UnitPushProvisioning.
+FOUNDATION_EXPORT const unsigned char UnitPushProvisioningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitPushProvisioning/PublicHeader.h>
+
+
