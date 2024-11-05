981bbb1fd22bdd13c36a2ed9528f3c5c8722f512
Alon Shprung <alonshp1@gmail.com>
2024-11-05 16:03:29 +0200

    add

---
+++ UnitCore.xcframework/ios-arm64/UnitCore.framework/Headers/UnitSDKCore.h
@@ -0,0 +1,18 @@
+//
+//  UnitSDKCore.h
+//  UnitSDKCore
+//
+//  Created by Alon Shprung on 05/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitSDKCore.
+FOUNDATION_EXPORT double UnitSDKCoreVersionNumber;
+
+//! Project version string for UnitSDKCore.
+FOUNDATION_EXPORT const unsigned char UnitSDKCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitSDKCore/PublicHeader.h>
+
+
